1c15b0c23fedd121c6e3cedc2913857e9e85ba6f
Matarov <moe.matar1998@gmail.com>
2023-01-23 00:12:11 +0200

    controllers done for personal info

--- Controllers/personalInfoController.js
+++ Controllers/personalInfoController.js
@@ -12,22 +12,44 @@ class Controller {
     //get By Id
     getById(req, res, next) {
         let { id } = req.params;
-        if (id.match(/^[0-9a-fA-F]{24}$/)) {
-            personalInfo.findOne({ _id: id }, (err, response) => {
-                if (err) return next(err);
-                res.status(200).json({ success: true, response });
-            });
-        } else
-            res.status(404).json({ success: false, response: "error" });
+        // if (id.match(/^[0-9a-fA-F]{24}$/)) {
+        personalInfo.findOne({ _id: id }, (err, response) => {
+            if (err) return next(err);
+            res.status(200).json({ success: true, response });
+        });
+        // } 
+        // else
+        //     res.status(404).json({ success: false, response: "error" });
     }
     //add
     post(req, res, next) {
-        // let { filename } = req.file;
-        let body = req.body;
-        // let body = { name: name, description: description, img: filename };
+        let { filename } = req.file;
+        let { name, headline, cv, aboutMe } = req.body;
+        let body = { name: name, headline: headline, cv: cv, aboutMe: aboutMe, image: filename };
 
         let doc = new personalInfo(body);
         doc.save((err, response) => {
+            if (err) return next(err);
+            res.status(200).send({ success: true, response });
+        });
+    }
+
+    //edit
+    put(req, res, next) {
+        let { id } = req.params;
+        let { name, headline, cv, aboutMe } = req.body;
+        let { filename } = req.file || {};
+        let data = { name: name, headline: headline, cv: cv, aboutMe: aboutMe, image: filename };
+        personalInfo.updateOne({ _id: id }, data, (err, response) => {
+            if (err) return next(err);
+            res.status(200).send({ success: true, response });
+        });
+    }
+
+    //delete
+    delete(req, res, next) {
+        let { id } = req.params;
+        personalInfo.findByIdAndDelete({ _id: id }, (err, response) => {
             if (err) return next(err);
             res.status(200).send({ success: true, response });
         });
--- routes/personalInfo.js
+++ routes/personalInfo.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const multer = require("multer");
 const controller = require("../Controllers/personalInfoController")
 const router = express.Router();
+
+const path = "public/uploads";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
+const upload = multer({ storage: storage });
 
 // Create Routes
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
-router.post("/", controller.post);
+router.post("/", upload.single("image"), controller.post);
+router.put("/:id", upload.single("image"), controller.put);
+router.delete("/:id", controller.delete);
 
 module.exports = router;
